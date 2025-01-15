08edbb7eb195a1353be7f9d28c1c5fcfa3a0ffdb
Matrosen <omkha7788@gmail.com>
2025-01-15 01:31:26 +0100

    Removed deprecated MongoDB options (useNewUrlParser, useUnifiedTopology)

--- app.js
+++ app.js
@@ -1,41 +1,71 @@
-    const express = require("express"); 
-    const app = express(); 
-    const port = 5000;
-    const mongoose = require("mongoose"); 
-    const routes = require("./server/routes/routes.js");
-    const bcryptjs = require('bcryptjs'); // Verwende bcryptjs statt bcrypt    
-    const session = require("express-session");
-    const fileUpload = require('express-fileupload');
-    const methodOverride = require('method-override');
-    app.use(methodOverride('_method'));
-    app.use(express()); 
-    app.use(fileUpload()); 
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+const mongoose = require("mongoose");
+const routes = require("./server/routes/routes.js");
+const bcryptjs = require('bcryptjs'); // Verwende bcryptjs statt bcrypt
+const session = require("express-session");
+const fileUpload = require('express-fileupload');
+const methodOverride = require('method-override');
+const MongoStore = require('connect-mongo');
 
-    require('dotenv').config();
+app.use(methodOverride('_method'));
+app.use(express());
+app.use(fileUpload());
 
-    app.use(express.static('public'));
-    app.set('view engine', 'ejs'); 
-    app.use(express.urlencoded({extended:false})); 
-    app.use(session({
-        secret: 'dein_geheimes_schlüsselwort', 
-        resave: false, 
-        saveUninitialized: false, 
-        cookie: { secure: false } 
-    }));app.use("/", routes); 
-    mongoose.connect("mongodb+srv://omkha7788:Dnd7kNWZhGlYh4TS@projectdb.5x0ub.mongodb.net/?retryWrites=true&w=majority&appName=ProjectDB")
-    .then(()=>{
-        console.log("Connected"); 
-    }).catch((error)=>{
-        console.log("error "  +error); 
-    })
-// App.js oder dein Haupt-Server-File
-app.use((req, res, next) => {
-    res.locals.user = req.session.check || null; // Macht 'user' global verfügbar
-    next();
+// Verwendet Umgebungsvariablen, falls diese gesetzt sind
+require('dotenv').config();
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: false}));
+
+// Konfiguriert die Session mit MongoDB Store
+app.use(session({
+  secret: 'dein_geheimes_schlüsselwort',
+  resave: false,
+  saveUninitialized: false,
+  store: MongoStore.create({
+    mongoUrl: 'mongodb+srv://omkha7788:Dnd7kNWZhGlYh4TS@projectdb.5x0ub.mongodb.net/?retryWrites=true&w=majority&appName=ProjectDB',
+  }),
+  cookie: { secure: false }
+}));
+
+app.use("/", routes);
+
+// Verbinde dich mit MongoDB (ohne die veralteten Optionen)
+mongoose.connect(
+  "mongodb+srv://omkha7788:Dnd7kNWZhGlYh4TS@projectdb.5x0ub.mongodb.net/?retryWrites=true&w=majority&appName=ProjectDB", 
+  { 
+    bufferCommands: false, // Deaktiviert das Pufferung von Abfragen
+    serverSelectionTimeoutMS: 5000, // Timeout für die Verbindung nach 5 Sekunden
+  })
+  .then(() => {
+    console.log("MongoDB verbunden");
+  })
+  .catch((error) => {
+    console.error("Fehler bei der MongoDB-Verbindung: " + error.message);
+  });
+
+// Bei erfolgreicher Verbindung MongoDB-Verbindung öffnen
+mongoose.connection.once('open', () => {
+  console.log("MongoDB-Verbindung erfolgreich hergestellt");
+
+  // Hier kannst du deine Abfragen durchführen, z.B.:
+  // Config.findOne({}).then(result => { console.log(result); }).catch(err => { console.error(err); });
 });
 
+mongoose.connection.on('error', (error) => {
+  console.error("Fehler bei der MongoDB-Verbindung: " + error);
+});
 
+// Macht 'user' global verfügbar
+app.use((req, res, next) => {
+  res.locals.user = req.session.check || null; // Macht 'user' global verfügbar
+  next();
+});
 
-    app.listen(port,()=>{
-        console.log("listening");
-    })
+// Server starten
+app.listen(port, () => {
+  console.log(`Server läuft auf Port ${port}`);
+});
